e949d61746171a30929c8fe5a32a1a3d97c21026
mohamed <mehdaoui200309@gmail.com>
2024-04-24 17:18:13 +0100

    "Add search functionality for groups and hide non-matching results"
    This commit adds a search feature for groups in the workspace view. It includes a new input field for searching and uses JavaScript to hide groups that do not match the search query

--- resources/views/workspace.blade.php
+++ resources/views/workspace.blade.php
@@ -16,6 +16,9 @@
     @vite('resources/css/app.css')
     <title>UniColab</title>
     <style>
+        .hide{
+            display: none
+        }
         .scrolling {
             overflow-y: auto;
         }
@@ -228,21 +231,19 @@
                 </p>
             </div>
             <div class=" w-full border-b flex shadow">
-                <form method="GET">
-                    <div class="relative text-gray-600">
-                        <span class="absolute inset-y-0 left-0 flex items-center pl-2">
-                            <button type="submit" class="p-1 focus:outline-none focus:shadow-outline">
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                    stroke-width="2" viewBox="0 0 24 24" class="w-6 h-6">
-                                    <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-                                </svg>
-                            </button>
-                        </span>
-                        <input type="text" name="q"
-                            class="py-2 text-sm text-white  rounded-md pl-10 w-72 focus:outline-none focus:bg-white focus:text-gray-900 h-10"
-                            placeholder="Search..." autocomplete="off">
-                    </div>
-                </form>
+                <div class="relative text-gray-600">
+                    <span class="absolute inset-y-0 left-0 flex items-center pl-2">
+                        <button type="submit" class="p-1 focus:outline-none focus:shadow-outline">
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                stroke-width="2" viewBox="0 0 24 24" class="w-6 h-6">
+                                <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                            </svg>
+                        </button>
+                    </span>
+                    <input type="text" name="q"
+                        class="py-2 text-sm text-white  rounded-md pl-10 w-72 focus:outline-none focus:bg-white focus:text-gray-900 h-10"
+                        placeholder="Search..." autocomplete="off" id="group_search">
+                </div>
             </div>
             <div class="scrolling h-fittall">
                 <!-- Projects Listing goes here -->
@@ -388,5 +389,16 @@
 <script>
     var elem = document.getElementById('{{$mainGroup->id}}');
     elem.scrollIntoView();
+    let data={{ Js::from($groups) }};
+    const groupInput=document.getElementById('group_search');
+    groupInput.addEventListener('input',e=>{
+        const value=e.target.value.toLowerCase()
+        data.forEach(user=>{
+            const isVisible=user.title.toLowerCase().includes(value)||user.company.toLowerCase().includes(value)
+            document.getElementById(user.id).classList.toggle('hide',!isVisible)
+            // console.log(isVisible);
+        })
+    })
+    // console.log(groupInput);
 </script>
 </html>
