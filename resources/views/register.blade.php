dc759cb59c9bc260095fc42d80aaa7d729921c5b
someone2042 <110247913+someone2042@users.noreply.github.com>
2024-07-18 11:03:41 +0100

    "Refactored register.blade.php: removed unnecessary whitespace and newline characters, and reformatted HTML and PHP code for consistency."

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,77 +1,50 @@
-<form  action="/users" method="POST">
+<form action="/users" method="POST">
     @csrf
     <div class="mb-6">
         <label for="name" class="inline-block text-lg mb-2">
             Name
         </label>
-        <input
-            type="text"
-            class="border border-gray-200 rounded p-2 w-full"
-            name="name"
-            value="{{old('name')}}"
-        />
+        <input type="text" class="border border-gray-200 rounded p-2 w-full" name="name"
+            value="{{ old('name') }}" />
         @error('name')
-            <p class="text-red-500 test-xs mt-1">{{$message}}</p>
+            <p class="text-red-500 test-xs mt-1">{{ $message }}</p>
         @enderror
     </div>
 
     <div class="mb-6">
-        <label for="email" class="inline-block text-lg mb-2"
-            >Email</label
-        >
-        <input
-            type="email"
-            class="border border-gray-200 rounded p-2 w-full"
-            name="email"
-            value="{{old('email')}}"
-        />
+        <label for="email" class="inline-block text-lg mb-2">Email</label>
+        <input type="email" class="border border-gray-200 rounded p-2 w-full" name="email"
+            value="{{ old('email') }}" />
         <!-- Error Example -->
         @error('email')
-            <p class="text-red-500 test-xs mt-1">{{$message}}</p>
+            <p class="text-red-500 test-xs mt-1">{{ $message }}</p>
         @enderror
     </div>
 
     <div class="mb-6">
-        <label
-            for="password"
-            class="inline-block text-lg mb-2"
-        >
+        <label for="password" class="inline-block text-lg mb-2">
             Password
         </label>
-        <input
-            type="password"
-            class="border border-gray-200 rounded p-2 w-full"
-            name="password"
-            value="{{old('password')}}"
-        />
+        <input type="password" class="border border-gray-200 rounded p-2 w-full" name="password"
+            value="{{ old('password') }}" />
         @error('password')
-            <p class="text-red-500 test-xs mt-1">{{$message}}</p>
+            <p class="text-red-500 test-xs mt-1">{{ $message }}</p>
         @enderror
     </div>
 
     <div class="mb-6">
-        <label
-            for="password2"
-            class="inline-block text-lg mb-2"
-        >
+        <label for="password2" class="inline-block text-lg mb-2">
             Confirm Password
         </label>
-        <input
-            type="password"
-            class="border border-gray-200 rounded p-2 w-full"
-            name="password_confirmation"
-            value="{{old('password_confirmation')}}"
-        />
+        <input type="password" class="border border-gray-200 rounded p-2 w-full" name="password_confirmation"
+            value="{{ old('password_confirmation') }}" />
         @error('password_confirmation')
-            <p class="text-red-500 test-xs mt-1">{{$message}}</p>
+            <p class="text-red-500 test-xs mt-1">{{ $message }}</p>
         @enderror
     </div>
 
     <div class="mb-6">
-        <button
-            type="submit"
-            class="bg-laravel text-white rounded py-2 px-4 hover:bg-black"
-        >
+        <button type="submit" class="bg-laravel text-white rounded py-2 px-4 hover:bg-black">
             Sign Up
         </button>
     </div>
@@ -79,9 +52,7 @@
     <div class="mt-8">
         <p>
             Already have an account?
-            <a href="/login" class="text-laravel"
-                >Login</a
-            >
+            <a href="/login" class="text-laravel">Login</a>
         </p>
     </div>
 </form>
